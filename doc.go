542e468a2e4d481628f3543771181e684884dac1
Logan Hanks <logan.hanks@gmail.com>
2015-04-16 11:47:07 -0700

    Add doc.go.

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package sketchy provides count- and rate-tracking sketches. These are
+probabilistic data structures that can track the occurrences of a very
+large number of distinct keys using a relatively small amount of space.
+The resulting counts/rates are estimates with a guaranteed level of
+accuracy.
+*/
+package sketchy
